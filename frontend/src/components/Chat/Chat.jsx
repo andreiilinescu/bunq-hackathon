11b35e4ce586b02a868c709eabbced92d72963b5
matei <matei.dumitrescu@gmail.com>
2025-05-10 15:10:40 +0200

    pushprod:fuck the git push script, I can't login to github from WSL

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -111,18 +111,19 @@ const Chat = () => {
 			}),
 		});
 		const responseData = await response.json();
-    
+
 		// Get the text message from the response
-		const textMessage = responseData.messages[responseData.messages.length - 1].text;
-		
+		const textMessage =
+			responseData.messages[responseData.messages.length - 1].text;
+
 		if (reciteMessages && responseData.audio) {
 			// Convert base64 audio to a playable format
 			const audioBase64 = responseData.audio;
-			const audioBlob = base64ToBlob(audioBase64, 'audio/mp3');
+			const audioBlob = base64ToBlob(audioBase64, "audio/mp3");
 			const audioUrl = URL.createObjectURL(audioBlob);
 			await reciteAudioFile(audioUrl);
 		}
-		
+
 		// Return the text message
 		console.log("textMessage", textMessage);
 		return textMessage;
@@ -132,19 +133,19 @@ const Chat = () => {
 	const base64ToBlob = (base64, mimeType) => {
 		const byteCharacters = atob(base64);
 		const byteArrays = [];
-		
+
 		for (let i = 0; i < byteCharacters.length; i += 512) {
 			const slice = byteCharacters.slice(i, i + 512);
 			const byteNumbers = new Array(slice.length);
-			
+
 			for (let j = 0; j < slice.length; j++) {
 				byteNumbers[j] = slice.charCodeAt(j);
 			}
-			
+
 			const byteArray = new Uint8Array(byteNumbers);
 			byteArrays.push(byteArray);
 		}
-		
+
 		return new Blob(byteArrays, { type: mimeType });
 	};
 	return (
